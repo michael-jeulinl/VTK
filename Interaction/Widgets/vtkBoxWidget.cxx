5388e2c689912f0253edd3017795a36237679997
Michael Jeulin-L <michael.jeulinl@kitware.com>
2012-07-30 14:56:17 -0400

    Add Picking Manager to deal with the picking process:
    Test interactively the functionalities with the example:
    - TestPickingManagerWidgets.cxx
    - TestPickingManagerSeedWidget.cxx
    
    Unit test added for the internal data structure && performances:
    - TestPickingManager.cxx
    - TestPickingManagerSeedWidget.cxx (Perfs)
    
    The management is currently implemented and enabled by default
    with the following widgets:
    - vtkBoxWidget
    - vtkBoxWidget2
    - vtkBrokenLineWidget
    - vtkBalloonWidget
    - vtkEllipsoidTensorProbeRepresentation
    - vtkImagePlaneWidget
    - vtkImageTracerWidget
    - vtkImplicitPlaneWidget
    - vtkLineWidget
    - vtkLineWidget2
    - vtkPlaneWidget
    - vtkProp3DButtonRepresentation
    - vtkSphereHandleRepresentation
    - vtkSphereWidget
    - vtkSphereWidget2
    - vtkSplineWidget
    - ...
    
    This effort is part of the MSV project.
    
    Change-Id: I9a92bed7c36903f56835b7e737f2aea6b7c1fc44
    
    Minor comment fixes to be merged within existing commit
    
    Change-Id: I5b25d9d9d960e5a5dea332c7af2a20a4e9756c28

--- Interaction/Widgets/vtkBoxWidget.cxx
+++ Interaction/Widgets/vtkBoxWidget.cxx
@@ -25,6 +25,7 @@
 #include "vtkFloatArray.h"
 #include "vtkMath.h"
 #include "vtkObjectFactory.h"
+#include "vtkPickingManager.h"
 #include "vtkPlanes.h"
 #include "vtkPolyData.h"
 #include "vtkPolyDataMapper.h"
@@ -566,9 +567,8 @@ void vtkBoxWidget::OnLeftButtonDown()
     return;
     }
 
-  vtkAssemblyPath *path;
-  this->HandlePicker->Pick(X,Y,0.0,this->CurrentRenderer);
-  path = this->HandlePicker->GetPath();
+  vtkAssemblyPath* path = this->GetAssemblyPath(X, Y, 0., this->HandlePicker);
+
   if ( path != NULL )
     {
     this->State = vtkBoxWidget::Moving;
@@ -579,8 +579,8 @@ void vtkBoxWidget::OnLeftButtonDown()
     }
   else
     {
-    this->HexPicker->Pick(X,Y,0.0,this->CurrentRenderer);
-    path = this->HexPicker->GetPath();
+    path = this->GetAssemblyPath(X, Y, 0., this->HexPicker);
+
     if ( path != NULL )
       {
       this->State = vtkBoxWidget::Moving;
@@ -642,10 +642,9 @@ void vtkBoxWidget::OnMiddleButtonDown()
     this->State = vtkBoxWidget::Outside;
     return;
     }
+  
+  vtkAssemblyPath* path = this->GetAssemblyPath(X, Y, 0., this->HandlePicker);
 
-  vtkAssemblyPath *path;
-  this->HandlePicker->Pick(X,Y,0.0,this->CurrentRenderer);
-  path = this->HandlePicker->GetPath();
   if ( path != NULL )
     {
     this->State = vtkBoxWidget::Moving;
@@ -656,8 +655,8 @@ void vtkBoxWidget::OnMiddleButtonDown()
     }
   else
     {
-    this->HexPicker->Pick(X,Y,0.0,this->CurrentRenderer);
-    path = this->HexPicker->GetPath();
+    path = this->GetAssemblyPath(X, Y, 0., this->HexPicker);
+
     if ( path != NULL )
       {
       this->State = vtkBoxWidget::Moving;
@@ -711,10 +710,9 @@ void vtkBoxWidget::OnRightButtonDown()
     this->State = vtkBoxWidget::Outside;
     return;
     }
+  
+  vtkAssemblyPath* path = this->GetAssemblyPath(X, Y, 0., this->HandlePicker);
 
-  vtkAssemblyPath *path;
-  this->HandlePicker->Pick(X,Y,0.0,this->CurrentRenderer);
-  path = this->HandlePicker->GetPath();
   if ( path != NULL )
     {
     this->State = vtkBoxWidget::Scaling;
@@ -724,8 +722,8 @@ void vtkBoxWidget::OnRightButtonDown()
     }
   else
     {
-    this->HexPicker->Pick(X,Y,0.0,this->CurrentRenderer);
-    path = this->HexPicker->GetPath();
+    path = this->GetAssemblyPath(X, Y, 0., this->HexPicker);
+
     if ( path != NULL )
       {
       this->State = vtkBoxWidget::Scaling;
@@ -1443,4 +1441,11 @@ void vtkBoxWidget::GenerateOutline()
     this->OutlineProperty->SetRepresentationToWireframe();
     this->SelectedOutlineProperty->SetRepresentationToWireframe();
     }
+}
+
+//------------------------------------------------------------------------------
+void vtkBoxWidget::RegisterPickers()
+{
+  this->Interactor->GetPickingManager()->AddPicker(this->HandlePicker, this);
+  this->Interactor->GetPickingManager()->AddPicker(this->HexPicker, this);
 }
